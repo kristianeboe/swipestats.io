1bbfefc2dc6b1644bdbee3a50b05df2a325676c7
Kristian Elset Bø <kristian.e.boe@gmail.com>
2024-06-27 11:42:08 +0200

    fix: Middleware experiment

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+
+// The country to block from accessing the secret page
+const BLOCKED_COUNTRY = "SE";
+
+// Trigger this middleware to run on the `/secret-page` route
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
+
+export default function middleware(request: NextRequest) {
+  // Extract country. Default to US if not found.
+  // const country = request.geo?.country;
+
+  // console.log(`Visitor from ${country}`);
+
+  // Rewrite to URL
+  return NextResponse.rewrite(request.url);
+}
