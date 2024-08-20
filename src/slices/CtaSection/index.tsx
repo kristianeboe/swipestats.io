52c6df095694542a858e28e3bf1fa9c6a25e9068
Kristian Elset Bø <kristian.e.boe@gmail.com>
2024-08-20 00:09:10 -0700

    fix: Colors in cta section

--- src/slices/CtaSection/index.tsx
+++ src/slices/CtaSection/index.tsx
@@ -28,7 +28,7 @@ const CtaSection = ({ slice }: CtaSectionProps): JSX.Element => {
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href={getPrismicLinkUrl(slice.primary.cta_href)}
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md bg-rose-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-rose-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-600"
               >
                 {slice.primary.cta_label}
               </a>
