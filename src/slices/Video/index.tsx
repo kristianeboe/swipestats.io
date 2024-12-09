493a8c545eb8b3e8ae4d45865ca8dd71c468f6c6
Kristian Elset Bø <kristian.e.boe@gmail.com>
2024-12-09 09:54:53 -0600

    fix: video slice

--- src/slices/Video/index.tsx
+++ src/slices/Video/index.tsx
@@ -11,6 +11,19 @@ export type VideoProps = SliceComponentProps<Content.VideoSlice>;
  */
 const Video = ({ slice }: VideoProps): JSX.Element => {
   if (!slice.primary.video_embed.html) return <div>No video to embed</div>;
+
+  const videoUrl = new URL(slice.primary.video_embed.embed_url);
+  const videoId = videoUrl.searchParams.get("v");
+  if (!videoId)
+    return (
+      <div>
+        <div>
+          No video ID found, make sure the embed url has a
+          &quot;?v=videoId&quot;
+        </div>
+        <div>Current embed url: {slice.primary.video_embed.embed_url}</div>
+      </div>
+    );
   return (
     <section
       data-slice-type={slice.slice_type}
@@ -23,7 +36,7 @@ const Video = ({ slice }: VideoProps): JSX.Element => {
           </div> */}
           <div className="aspect-video">
             <iframe
-              src={`https://www.youtube.com/embed/${"gotvpCDEzdU"}`}
+              src={`https://www.youtube.com/embed/${videoId}`}
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
               className="h-full w-full"
