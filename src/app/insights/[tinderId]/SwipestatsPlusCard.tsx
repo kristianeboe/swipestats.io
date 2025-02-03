32f550cd41d4c40a679b100c3cb2460112263078
Kristian Elset Bø <kristian.e.boe@gmail.com>
2025-02-02 23:23:26 -0800

    fix: Open lemon squeezy checkout in new tab

--- src/app/insights/[tinderId]/SwipestatsPlusCard.tsx
+++ src/app/insights/[tinderId]/SwipestatsPlusCard.tsx
@@ -293,6 +293,7 @@ export function SwipestatsPlusCard({ className }: { className?: string }) {
                   getProductData("swipestatsPlus").checkoutUrl +
                   `?checkout[custom][tinderId]=${myTinderId}`
                 }
+                target="_blank"
                 className="mt-10 block w-full rounded-md bg-rose-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-rose-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-600"
               >
                 Get access
